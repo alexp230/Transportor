df5d58da9106814107c7ed56de00385ed5b9f56e
alexp230 <apruitt230@gmail.com>
2025-01-11 13:43:35 -0600

    Added a progress bar.

--- Transparentor.py
+++ Transparentor.py
@@ -9,6 +9,9 @@ def MakeImageTransparent(inputPath: str, outputPath: str, backgroundColors: list
     def inRange(pixel, color_range):
         return all(color_range[i][0] <= pixel[i] <= color_range[i][1] for i in range(4))
     
+    allPixels = imageData.shape[0] * imageData.shape[1]
+    pixelCount = 0
+
     imageCopy = imageData.copy()
 
     # Loop through all pixels in image
@@ -23,6 +26,9 @@ def MakeImageTransparent(inputPath: str, outputPath: str, backgroundColors: list
                     break
             else:
                 imageCopy[y, x] = [255,255,255,255]
+            
+            pixelCount += 1
+            printProgressBar(pixelCount, allPixels)
 
     # Create a new image from the modified copy and make new file
     Image.fromarray(imageCopy, mode="RGBA").save(outputPath)
@@ -41,14 +47,46 @@ def PrintPixelsOfImage(imagePath):
             r, g, b = pixels[x, y]
             print(f"Pixel ({x}, {y}): R={r}, G={g}, B={b}")
 
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    @params:
+        iteration   - Required  : current iteration (int)
+        total       - Required  : total iterations (int)
+        prefix      - Optional  : prefix string (str)
+        suffix      - Optional  : suffix string (str)
+        decimals    - Optional  : positive number of decimals in percent complete (int)
+        length      - Optional  : character length of bar (int)
+        fill        - Optional  : bar fill character (str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (str)
+    """
+    def CalculateBar():
+        percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+        filledLength = int(length * iteration // total)
+        bar = fill * filledLength + '-' * (length - filledLength)
+        print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+
+    if (iteration == total):
+        CalculateBar()
+        print("\n")
+        return
+
+    if (iteration % 175 != 0):
+        return
+    
+    CalculateBar()    
+
 
 # Ranges of colors of potential backgrounds
 Gray = [(150, 250), (150, 250), (150, 250), (100, 255)]
 White = [(250, 255), (250, 255), (250, 255), (100, 255)]
 Black = [(0, 10), (0, 10), (0, 10), (250, 255)]
 
+fileName = "Number4"
+fileExtension = "png"
 
-fileInput = "Number6.png"
-fileOutput = "Number6New.png"
+fileInput = f"Inputs/{fileName}.{fileExtension}"
+fileOutput = f"Outputs/{fileName}-new.{fileExtension}"
 
+print()
 MakeImageTransparent(fileInput, fileOutput, [White, Gray])
